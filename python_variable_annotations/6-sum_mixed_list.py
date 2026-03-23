9555f3545edd5f1eb940097d7a58de7a42ce74e4
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-23 11:08:44 +0000

    change docstring

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -4,5 +4,5 @@ from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """returns sum of a list of integers and floats"""
+    """Return the sum of a list of integers and floats as a float."""
     return sum(mxd_lst)
--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,5 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """returns sum of a list of integers and floats"""
+    """Return a tuple of a string k and the square of int/float v as a float."""
     return (k, v ** 2)
