7ef4ac49646b469e47ee9df27efd38a4205daef7
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-23 10:41:24 +0000

    Complex types - list of floats

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that provided function sum_list."""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns sum of a list of floats"""
+    return sum(input_list)
