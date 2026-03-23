669b654dc6e265e6a84878196190b5368620c646
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-23 10:15:48 +0000

    Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module that provides a type-annotated addition function for two floats."""
+
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floats a and b."""
+    return a + b
