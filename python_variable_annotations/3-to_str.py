5e469cd33ddbdc002fceea64cfd38e2367ee0d2f
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-23 10:25:51 +0000

    Basic annotations - to string

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module that provided function to_str"""
+import math
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of the float"""
+    return str(n)
