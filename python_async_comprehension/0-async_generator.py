87036262e73637b4a589d114934bd4bd64728e39
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-30 04:20:26 +0000

    type hint a generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,9 @@
 """Module for async generator"""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Yields random numbers asynchronously."""
     for _ in range(10):
         await asyncio.sleep(1)
