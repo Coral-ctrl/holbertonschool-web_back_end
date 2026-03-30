a769ddec37383c79eb00cbf64b006bc19ba8d5e4
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-30 03:45:29 +0000

    Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Module for running multiple tasks"""
+
+import asyncio
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns task_wait_random n times and returns sorted delays."""
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+
+    for completed in asyncio.as_completed(tasks):
+        result = await completed
+        delays.append(result)
+
+    return delays
