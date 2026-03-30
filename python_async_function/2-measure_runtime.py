0135768bd3920e2989045b9a1aebf5c39f39bffc
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-03-30 03:20:44 +0000

    Measure the runtime

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module to measure runtime of async functions"""
+import asyncio
+import time
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Measures average execution time for wait_n."""
+    start_time = time.time()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    return total_time / n
