d0865f738a9296e95a9d45ad5e8e72d23e9cb2fc
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-04-13 07:04:21 +0000

    codestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -8,7 +8,9 @@ from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return a tuple containing the start and end indexes for a given page."""
+    """
+    Return a tuple containing the start and end indexes for a given page.
+    """
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return (start_index, end_index)
@@ -34,7 +36,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return a page of the dataset based on page number and page size."""
+        """
+        Return a page of the dataset based on page number and page size.
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
