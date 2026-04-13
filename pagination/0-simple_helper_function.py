effcc1e69976fa9657fea528777ba348b13c4bb4
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-04-13 06:11:46 +0000

    Simple helper function

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module providing a simple helper function for pagination index calculation."""
+
+from typing import Tuple
+
+
+def index_range(page: int, : int) -> Tuple[int, int]:
+    """Return a tuple containing the start and end indexes for a given page."""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
