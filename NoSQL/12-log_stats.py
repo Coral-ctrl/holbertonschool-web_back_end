a4f40cf5511ad87426020998f6ba6b05cc65b773
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-04-19 01:44:45 +0000

    restructure

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -3,17 +3,26 @@
 from pymongo import MongoClient
 
 
-if __name__ == "__main__":
+def log_stats():
+    """Display stats about Nginx logs in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
 
     total = collection.count_documents({})
-    print(f"{total} logs")
+    print(total, "logs")
 
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+    for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print(status_check, "status check")
+
+
+if __name__ == "__main__":
+    log_stats()
