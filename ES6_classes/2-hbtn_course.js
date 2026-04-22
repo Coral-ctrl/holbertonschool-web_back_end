debc448f35950b03a7cf5abe83a04bc3d28fe9e3
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-04-22 12:05:05 +0000

    fix typo

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,7 +1,7 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
         if (typeof name !== 'string') throw new TypeError('Name must be a string');
-        if (typeof length !== 'number') throw new TypeError('Length must be a number);
+        if (typeof length !== 'number') throw new TypeError('Length must be a number');
         if (!Array.isArray(students) || !students.every((s) => typeof s === 'string')) {
             throw new TypeError('Students must be an array of strings');
         }
