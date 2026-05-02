16176a9e42fd3b82dc9842649d20d6e69dc3ed82
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-02 12:25:38 +0000

    Handle multiple promises

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,11 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+ 
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  // Promise.allSettled([...]) — waits for every promise to settle (fulfilled or rejected)
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((results) => results.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason,
+    })));
+}
