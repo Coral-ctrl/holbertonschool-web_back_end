a0017bec57414ba5d10790b017ef86349154bb7c
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-04 06:51:20 +0000

    Create a more complex HTTP server using Express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const fs = require('fs');
+ 
+// Same countStudents logic from 5-http.js - builds output as a string
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+ 
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1);
+ 
+      let output = `Number of students: ${students.length}`;
+ 
+      const fields = {};
+      for (const student of students) {
+        const [firstname, , , field] = student.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+ 
+      for (const [field, names] of Object.entries(fields)) {
+        output += `\nNumber of students in ${field}: ${names.length}. List: ${names.join(', ')}`;
+      }
+ 
+      resolve(output);
+    });
+  });
+}
+ 
+// Database file passed as command line argument
+const database = process.argv[2];
+ 
+const app = express();
+ 
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+ 
+app.get('/students', (req, res) => {
+  countStudents(database)
+    .then((output) => {
+      res.send(`This is the list of our students\n${output}`);
+    })
+    .catch((err) => {
+      res.send(`This is the list of our students\n${err.message}`);
+    });
+});
+ 
+app.listen(1245);
+ 
+module.exports = app;
