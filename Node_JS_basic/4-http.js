92b2c4fffdecc944bf2c52743383366ebd26153e
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-04 06:20:44 +0000

    Create a small HTTP server using Node's HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+    // Set response header to plain text for any endpoint
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    // Send the same response body regardless of the URL path
+    res.end('Hello Holberton School1');
+});
+
+// Start listening on port 1245
+app.listen(1245);
+
+module.exports = app;
