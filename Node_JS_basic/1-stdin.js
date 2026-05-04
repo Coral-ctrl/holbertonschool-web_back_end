7ef893a16fa3789f1bfd159eb5e575b9c1e23df7
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-04 05:03:10 +0000

    Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data}`);
+});
+
+process.stdin.on('close', () => {
+    process.stdout.write('This important software is now closing\n');
+});
