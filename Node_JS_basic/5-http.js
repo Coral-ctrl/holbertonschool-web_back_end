a41faf523ef3d1166d2781a660a38712d45a1116
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-07 02:43:45 +0000

    fix typo

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -39,7 +39,7 @@ const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
 
   if (req.url === '/') {
-    res.end('Hello Holberton School1');
+    res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     // Start the response with the header line
     countStudents(database)
