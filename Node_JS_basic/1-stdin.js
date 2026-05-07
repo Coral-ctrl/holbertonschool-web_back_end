1b41baec7b703091cf7a68fea712fb1000e16f6c
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-07 01:17:57 +0000

    use end instead

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -7,6 +7,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
-process.stdin.on('close', () => {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
