57d6f103aeed07a974bda82753f657c58e4861bd
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-07 02:41:11 +0000

    fix typo

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -4,7 +4,7 @@ const app = http.createServer((req, res) => {
   // Set response header to plain text for any endpoint
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   // Send the same response body regardless of the URL path
-  res.end('Hello Holberton School1');
+  res.end('Hello Holberton School!');
 });
 
 // Start listening on port 1245
