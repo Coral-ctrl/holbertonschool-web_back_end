f2d578b276987f23768065048c846cc18ca2e46f
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-07 01:08:39 +0000

    fix ESLint issues

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,9 +4,9 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data}`);
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('close', () => {
-    process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
