073af4d6239772885d5ab0e51925c824093a3f88
“Coral-ctrl” <liangyongshan33@gmail.com>
2026-05-07 01:57:29 +0000

    fix eslint issues

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
 const app = http.createServer((req, res) => {
-    // Set response header to plain text for any endpoint
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    // Send the same response body regardless of the URL path
-    res.end('Hello Holberton School1');
+  // Set response header to plain text for any endpoint
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Send the same response body regardless of the URL path
+  res.end('Hello Holberton School1');
 });
 
 // Start listening on port 1245
